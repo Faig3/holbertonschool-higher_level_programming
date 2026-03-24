6cc5bae130d35935a0ab1bb20f367f200ce2a5cb
Faig3 <13603@holbertonstudents.com>
2026-03-24 14:46:26 +0000

    2

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,10 @@
+#!/user/bin/python3
+from 1-calculation.py import add, sub, mul, div
+
+if __name__ = "__main__":
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} - {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    Print("{} / {} = {}".format(a, b, div(a, b)))
