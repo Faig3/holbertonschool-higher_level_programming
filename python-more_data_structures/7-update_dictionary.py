301aac5d0006c88ce911ca83cbbb282e75bef5f2
Faig3 <13603@holbertonstudents.com>
2026-03-30 18:37:36 +0000

    7

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -3,4 +3,4 @@
 
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
-    [print("{}: {}".format(k, a_dictionary[k]), for k in sorted(a_dictionary)]
+    return a_dictionary
