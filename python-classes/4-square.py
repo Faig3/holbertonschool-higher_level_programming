6cc30bbb6ceff823afc2d38d277749982e35f82b
Faig3 <13603@holbertonstudents.com>
2026-04-04 13:31:08 +0000

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -13,6 +13,7 @@ class Square:
     def size(self):
         """Retrieves the size of the square."""
         return self.__size
+
     @size.setter
     def size(self, value):
         """Sets the size of the square."""
@@ -21,6 +22,7 @@ class Square:
         if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
+
     def area(self):
         """Returns the current square area."""
-        return self.__size ** 
+        return self.__size ** 2
