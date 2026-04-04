07aa4463f0b59a121faaa0a7a0eb1905487828bd
Faig3 <13603@holbertonstudents.com>
2026-04-04 13:13:25 +0000

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -1,13 +1,14 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 """Module for Square class."""
 
 
 class Square:
     """Defines a square."""
+
     def __init__(self, size=0):
-         """Initializes the square size."""
-         if not instance(size, int):
-             raise TypeError("size must be an integer")
-         if size < 0:
-             raise ValueError("size must >= 0")
-         self.__size = size
+        """Initializes the square size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
