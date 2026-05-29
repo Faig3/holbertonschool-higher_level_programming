a4e127065677cf0fe8f5cc259a27d53deb23b625
Faig3 <13603@holbertonstudents.com>
2026-05-29 12:49:13 +0000

    7

--- SQL_more_queries/7-cities.sql
+++ SQL_more_queries/7-cities.sql
@@ -2,7 +2,7 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (
-       id INT NOT NULL AUTO_INCREMENT ,
+       id INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT ,
        state_id INT NOT NULL ,
        name VAR_CHAR(256) NOT NULL,
        PRIMARY KEY (id),
